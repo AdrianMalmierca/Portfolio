380fa7e011035c75780addb126efab68a41dd97e
AdeulianSuke <>
2026-03-02 18:34:42 +0100

    modified style on mobile

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,28 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { HeroComponent } from "../../components/hero/hero.component";
 import { PortfolioComponent } from "../../components/portfolio/portfolio.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { ExperienceComponent } from "../../components/experience/experience.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, TranslateModule, HeroComponent, PortfolioComponent, FooterComponent, ProjectsComponent, ExperienceComponent],
+  imports: [HeroComponent, PortfolioComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('fr');
-    this.translate.use('fr');
-  }
-
-  switchLang(lang: string) {
-    this.translate.use(lang);
-  }
 }
